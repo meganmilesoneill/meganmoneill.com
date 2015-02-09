3a550428fe46cbe62578c752d04543ca13aa3464
meganmilesoneill <meganmilesoneill@gmail.com>
2015-02-09 01:27:19 -0800

    added Vagrant and data folder

---
+++ vagrant_provision.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y apache2
+
+sudo apt-get install -y vim unzip curl
+
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+# sudo apt-get install -y oracle-java8-installer
+
+# sudo curl -L -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.2.zip
+# sudo unzip elasticsearch-1.4.2.zip
+# sudo cp -r elasticsearch-1.4.2 /usr/local
+# cd /usr/local/elasticsearch-1.4.2
+#sudo ./bin/plugin -i elasticsearch/marvel/latest
+#sudo ./bin/elasticsearch -d
