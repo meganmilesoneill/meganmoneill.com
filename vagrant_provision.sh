3f832f9c5a8ad094a4a0880fecd4a18155ca754f
meganmilesoneill <meganmilesoneill@gmail.com>
2015-02-10 23:23:13 -0800

    adding pip install and elasticsearch python library

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y vim unzip curl
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
+sudo apt-get install -y python-setuptools python-dev build-essential 
+sudo easy_install pip 
+sudo pip install --upgrade virtualenv 
 # sudo apt-get install -y oracle-java8-installer
 
 # sudo curl -L -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.2.zip
@@ -14,3 +17,4 @@ sudo apt-get update
 # cd /usr/local/elasticsearch-1.4.2
 #sudo ./bin/plugin -i elasticsearch/marvel/latest
 #sudo ./bin/elasticsearch -d
+#sudo pip install elasticsearch
